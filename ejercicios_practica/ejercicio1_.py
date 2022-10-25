b4230a7e64efaa4b633daa0462fe5274d72ba10a
edwinyepezn <111158225+edwinyepezn@users.noreply.github.com>
2022-10-25 13:23:38 -0500

    ejercicios resueltos

--- ejercicios_practica/ejercicio1_.py
+++ ejercicios_practica/ejercicio1_.py
@@ -25,7 +25,30 @@ def serializar():
     #  { "prenda": "remeras", "cantidad": 12 }
     # Que su lista de prendas dentro del JSON tenga al menos 2 prendas
 
-    # json_data = {...}
+    json_data = {
+                "Nombre": "Edwin",
+                "Apellido": "Yepez",
+                "DNI": "152329",
+                "Vestir": [
+                        {
+                            "prenda": "camisa",
+                            "cantidad": 12
+                        },
+                        {
+                            "prenda": "pantalon",
+                            "cantidad": 8
+                        },
+                        {
+                            "prenda": "chaqueta",
+                            "cantidad": 4
+                        }
+                        ]
+                }
+    # print(json_data)
+    with open ('edwin_json.json', 'w') as archivo_json:
+        datos = [json_data]
+        json.dump(datos, archivo_json, indent=4)
+        
 
     # Una vez que finalice el JSON realice un "dump" para almacenarlo en
     # un archivo que usted defina
@@ -39,7 +62,20 @@ def deserializar():
     # Basado en la función  anterior debe abrir y leer el contenido
     # del archivo y guardarlo en un objeto JSON utilizando el método
     # load()
+    json_data = {"Autos": "mazda", "motocicletas": "yamaha"}
+    
+    with open('edwin_json.json', 'r') as otro_json:
+        nueva_data = json.load(otro_json)
+        nueva_data.append(json_data)
 
+    with open('edwin_json.json', 'w') as otro_json:
+        json.dump(nueva_data, otro_json, indent=4)
+
+    with open('edwin_json.json', 'r') as otro_json:
+        json_data = json.load(otro_json)
+
+    print('Mostrar el contenido del archivo mi_json')
+    print(json.dumps(json_data, indent=4))
     # Luego debe convertir ese JSON data en json_string utilizando
     # el método "dumps" y finalmente imprimir en pantalla el resultado
     # Recuerde utilizar indent=4 para poder observar mejor el resultado
@@ -50,5 +86,5 @@ if __name__ == '__main__':
     
     serializar()
     deserializar()
-
+    
     print("terminamos")
--- ejercicios_practica/ejercicio2.py
+++ ejercicios_practica/ejercicio2.py
@@ -1,13 +1,15 @@
 # JSON ETL [Python]
 # Ejercicios de práctica
 
-# Autor: Inove Coding School
+# Autor: Edwin Yepez N.
 # Version: 2.0
 
 # IMPORTANTE: NO borrar los comentarios
 # que aparecen en verde con el hashtag "#"
 
 import json
+from typing import Counter
+import black
 import requests
 
 import matplotlib.pyplot as plt
@@ -27,8 +29,38 @@ if __name__ == '__main__':
     # del userId=1 al userId=10
     # 3) En cada entrada se especifica si el usuario completó ese título,
     # mediante el campo "completed".
+    respuesta = requests.get(url)
+    datos = json.loads(respuesta.text)
+    datos = respuesta.json()
+    print('Imprimir los datos traídos de la nube')
+    print(json.dumps(datos, indent=4))
+    respuesta = requests.get(url)
+    datos = respuesta.json()
+    conteo = []
+    a = 0
+    for j in range(10):
+        for i in datos:
+            if i['userId'] == j+1:
+                if i['completed'] == True:
+                    a = a + 1
+        conteo.append(a)
+        a = 0                        
+    
+    usuario = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    print('Los datos extraidos a graficar son: ', conteo)
+
+    fig = plt.figure()
+    fig.suptitle('Títulos Leidos porcada usuario', fontsize=16)
+    
+    ax = fig.add_subplot()
 
 
+    ax.bar(usuario, conteo, label='Títulos')
+    ax.set_facecolor('whitesmoke')
+    ax.legend()
+    plt.show()
+
+    
     # Alumno, de cada usuario en el total de las 200 entradas
     # debe contar cuantos títulos completó cada usuario (de los 10 posibles)
     # y armar un gráfico de barras resumiendo la información.
